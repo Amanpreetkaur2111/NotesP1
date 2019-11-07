5b906345a0b66cb555667d5f4a3dd72c55dc2553
AmanpreetKaur <aman021011@gmail.com>
2019-11-07 16:39:48 -0500

    edited files

--- Amanpreet_766345_Notes_P1/ViewController.swift
+++ Amanpreet_766345_Notes_P1/ViewController.swift
@@ -8,26 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+   
+    @IBOutlet weak var tableView: UITableView!
+    var folders: [String]?
+override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        self.navigationItem.rightBarButtonItem = editButtonItem
+     
+    self.navigationItem.rightBarButtonItem = editButtonItem
+     folders = []
     }
 
     @IBAction func NewFolder(_ sender: UIBarButtonItem) {
-let alertaction = UIAlertController(title: "New Folder", message: "Enter a name for this folder", preferredStyle: .actionSheet)
-    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
-let addItemAction = UIAlertAction(title: "AddItem", style: .cancel, handler: nil)
-alertaction.addAction(cancelAction)
-self.present(alertaction, animated: true, completion: nil)
-alertaction.addAction(addItemAction)
-self.present(alertaction, animated: true, completion: nil)
-        
+let alertaction = UIAlertController(title: "New Folder", message: "Enter a name for this folder", preferredStyle: .alert)
+         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertaction.addTextField { (textField) in textField.placeholder = "Name"
+            
+        }
+        alertaction.addAction(UIAlertAction(title: "Add Item", style: .destructive, handler: {(action) in
+    let Name = alertaction.textFields?.first?.text
+             print("\(Name)")
+            self.folders?.append(Name!)
+           self.tableView.reloadData()
+            print("\(self.folders)")
+            
+        }))
+       
+      alertaction.addAction(cancelAction)
+      self.present(alertaction, animated: true, completion: nil)
+
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return folders?.count ?? 0
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "")
+        cell.textLabel?.text = folders![indexPath.row]
+        cell.imageView?.image = UIImage(named: "folder")
+        return cell
+    }
+    
     
 }
 
